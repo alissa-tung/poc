61e2b06e6569dad3c09dd821f42392728498c1f4
Alissa Tung <alissa@mail.ncut.edu.cn>
2022-08-08 00:04:33 +0800

    poc-hs: generic rs vec

--- poc/poc-hs/src/PB/Common/Containers.hs
+++ poc/poc-hs/src/PB/Common/Containers.hs
@@ -1,5 +1,6 @@
 module PB.Common.Containers where
 
+import Data.Foldable
 import Data.Functor
 import Data.Map.Strict qualified as Map
 import Foreign
@@ -11,12 +12,39 @@ data RsHashMap rsk rsv
 fromRsHashMap :: forall rsk rsv hsk hsv. (RsFFI rsk hsk, RsFFI rsv hsv) => Ptr (RsHashMap rsk rsv) -> IO (Map.Map hsk hsv)
 fromRsHashMap = undefined
 
-toRsHashMap :: forall hsk hsv rsk rsv. (RsFFI rsk hsk, RsFFI rsv hsv) => Map.Map hsk hsv -> IO (Ptr (RsHashMap rsk rsv))
+toRsHashMap :: forall rsk rsv hsk hsv. (RsFFI rsk hsk, RsFFI rsv hsv) => Map.Map hsk hsv -> IO (Ptr (RsHashMap rsk rsv))
 toRsHashMap = undefined
 
-fromRsVec :: RsFFI rst hst => Ptr (Ptr rst) -> Int -> IO [hst]
-fromRsVec (ptr :: Ptr (Ptr rst)) len =
-  sequence $ [0 .. len] <&> \i -> fromRs @rst (ptr `plusPtr` i)
+fromRsVec :: RsFFI rst hst => Ptr rst -> Int -> IO [hst]
+fromRsVec (ptr :: RsFFI rst hst => Ptr rst) len =
+  sequence $ [0 .. len] <&> fromRs @rst @hst . plusPtr ptr
 
-toRsVec :: RsFFI rst hst => [hst] -> IO (Ptr (Ptr rst), CSize)
+toRsVec :: RsFFI rst hst => [hst] -> IO (Ptr rst, CSize)
 toRsVec = undefined
+
+data RsVec a
+
+foreign import ccall
+  rs_vec_new :: Ptr a -> CSize -> IO (Ptr (RsVec a))
+
+foreign import ccall
+  rs_vec_get_ptr :: Ptr (RsVec a) -> IO (Ptr a)
+
+foreign import ccall
+  rs_vec_get_len :: Ptr (RsVec a) -> IO CSize
+
+instance RsDrop a => RsDrop (RsVec a) where
+  dropRs (xs :: RsDrop a => Ptr (RsVec a)) = do
+    ptr <- rs_vec_get_ptr xs
+    len <- fromIntegral <$> rs_vec_get_len xs
+    traverse_ (dropRs @a . plusPtr ptr) [0 .. len]
+
+instance RsFFI rst hst => RsFFI (RsVec rst) [hst] where
+  fromRs (rs :: RsFFI rst hst => Ptr (RsVec rst)) = do
+    ptr <- castPtr @_ @rst <$> rs_vec_get_ptr rs
+    len <- fromIntegral <$> rs_vec_get_len rs
+    fromRsVec @rst @hst ptr len
+
+  toRs (hs :: RsFFI rst hst => [hst]) = do
+    (ptr, len) <- toRsVec @rst @hst hs
+    rs_vec_new ptr len
--- poc/poc-hs/src/Poc/Gen/PB.hs
+++ poc/poc-hs/src/Poc/Gen/PB.hs
@@ -1,9 +1,7 @@
 module Poc.Gen.PB where
 
-import Control.Monad
 import Data.Map.Strict qualified as Map
 import Foreign
-import Foreign.C.Types
 import PB.Common
 import Poc.Gen.PB.OMsg qualified as OMsg
 
@@ -25,13 +23,10 @@ foreign import ccall
 
 foreign import ccall
   new_OMsg ::
-    Ptr (Ptr OMsg.RsIMsg) -> CSize -> Ptr (RsHashMap RsString RsString) -> IO (Ptr RsOMsg)
+    Ptr (RsVec OMsg.RsIMsg) -> Ptr (RsHashMap RsString RsString) -> IO (Ptr RsOMsg)
 
 foreign import ccall
-  get_OMsg_xs_ptr :: Ptr RsOMsg -> IO (Ptr (Ptr OMsg.RsIMsg))
-
-foreign import ccall
-  get_OMsg_xs_len :: Ptr RsOMsg -> IO CSize
+  get_OMsg_xs :: Ptr RsOMsg -> IO (Ptr (RsVec OMsg.RsIMsg))
 
 foreign import ccall
   get_OMsg_ys_ptr :: Ptr RsOMsg -> IO (Ptr (RsHashMap RsString RsString))
@@ -46,15 +41,16 @@ instance RsDrop RsOMsg where
 
 instance RsFFI RsOMsg OMsg where
   fromRs rs = do
-    xs <- join $ fromRsVec <$> get_OMsg_xs_ptr rs <*> (fromIntegral <$> get_OMsg_xs_len rs)
+    xs <- fromRs @(RsVec OMsg.RsIMsg) @[OMsg.IMsg] =<< get_OMsg_xs rs
     ys <-
       fromRsHashMap @RsString @RsString @(PBByteString PBString) @(PBByteString PBString)
         =<< get_OMsg_ys_ptr rs
     pure $ OMsg xs ys
 
   toRs (OMsg xs ys) = do
-    (xsPtr, xsLen) <- toRsVec xs
-    new_OMsg xsPtr xsLen =<< toRsHashMap ys
+    xs <- toRs @(RsVec OMsg.RsIMsg) @[OMsg.IMsg] xs
+    ys <- toRsHashMap @RsString @RsString @(PBByteString PBString) @(PBByteString PBString) ys
+    new_OMsg xs ys
 
 data RsPocMsg
 
